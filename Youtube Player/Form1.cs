a7a44f91d9f2e830701b88e7f16f6cf028b5fb62
Hristo <hristokehayov@gmail.com>
2019-07-15 22:32:48 +0200

    fixed some bugs and added new features

--- Youtube Player/Form1.cs
+++ Youtube Player/Form1.cs
@@ -13,118 +13,96 @@ using System.Windows.Forms;
 using VideoLibrary;
 using YoutubeSearch;
 using System.Media;
-
-
+using System.Text.RegularExpressions;
 
 namespace Youtube_Player
 {
     public partial class Form1 : Form
     {
+        //string url;
+        //public string VideoID
+        //{
+        //    get
+        //    {
+        //        var yMatch = new Regex(@"http(?:s?)://(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w\-]+)(&(amp;)?[\w\?=]*)?").Match(_yUrl);
+        //        return yMatch.Success ? yMatch.Groups[1].Value : string.Empty;
+        //    }
+        //}
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        // configure this code
-        //protected override void OnLoad(EventArgs e)
-        //{
-        //    base.OnLoad(e);
-        //    var embed = "<html><head>" +
-        //    "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\"/>" +
-        //    "</head><body>" +
-        //    "<iframe width=\"300\" src=\"{0}\"" +
-        //    "frameborder = \"0\" allow = \"autoplay; encrypted-media\" allowfullscreen></iframe>" +
-        //    "</body></html>";
-        //    var url = "https://www.youtube.com/embed/L6ZgzJKfERM";
-        //    this.webBrowser1.DocumentText = string.Format(embed, url);
-        //}
-
-        int youtubePageNumber = 1;
-
-        int index_of_youtubeVideos_List = 0;
-
-        private void Search_Btn_Click(object sender, EventArgs e)
+        public static string StripTagsRegex(string source)
         {
-            YoutubeInfo video_info = new YoutubeInfo(); // MAIN list of information for only 1 youtube video from page 1
+            return Regex.Replace(source, @"<div id=\'yt-masthead-signin\'>(\w.*)</div>", string.Empty);
+        }
 
-            VideoSearch items = new VideoSearch(); // youtube video search plugin
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //StripTagsRegex(webBrowser1.DocumentText);
+            webBrowser1.Navigate(textBox1.Text);
+            //string htmlofwebpage = webBrowser1.DocumentText;
+            //string pattern = @"<button id=\'appbar-guide-button\'>(\d.*)</button>";
+            //string pattern2 = @"(<\w.*)\w +";
+            //string g = "(\n.*)";
 
-            List<YoutubeInfo> youtube_Videos_List = new List<YoutubeInfo>(); // a list of the info's for specific video
+            //string pattern123 = @"<button class=\'yt - uix - button yt - uix - button - size - default yt - uix - button - text yt - uix - button - empty yt - uix - button - has - icon appbar - guide - toggle appbar - guide - clickable - ancestor\' type=\'button\' onclick=\'; return false;  id=\'appbar - guide - button\' aria-label=\'Guide\' aria-controls=\'appbar - guide - menu\'><span class=\'yt - uix - button - icon - wrapper\'><span class=\'yt - uix - button - icon yt - uix - button - icon - appbar - guide yt - sprite\'></span></span></button>";
+            //string part = @"<a id=\'logo - container\' href=\'  title=\'YouTube home\' class=\' spf - link  masthead - logo - renderer yt - uix - sessionlink\' data-sessionlink=""><span class=\'logo masthead-logo - renderer - logo yt - sprite\' title=\'YouTube home\'></span><span class=\'content - region\'>AT</span></a>";
 
-            foreach (var item in items.SearchQuery(textBox1.Text, youtubePageNumber))
+            //htmlofwebpage = Regex.Replace(htmlofwebpage, part, string.Empty);
+            //webBrowser1.DocumentText = htmlofwebpage;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
             {
-                video_info = new YoutubeInfo(); // separate queries from others queries // make different info holder for each video
-
-                video_info.title = item.Title;
-                video_info.Authur = item.Author;
-                video_info.url = item.Url.Replace("watch?v=", "embed/");
-                byte[] imageByte = new WebClient().DownloadData(item.Thumbnail);
-                using (MemoryStream ms = new MemoryStream(imageByte))
-                {
-                    video_info.thumbnail = Image.FromStream(ms);
-                }
-                youtube_Videos_List.Add(video_info);
+                Hide();
+                notifyIcon1.Visible = true;
             }
-            
-            CurrMusic_thumbnail.Image = youtube_Videos_List[0].thumbnail;
-            CurrTitle_of_theMusic.Text = youtube_Videos_List[0].title;
-            CurrAuthur_of_theMusic.Text = youtube_Videos_List[0].Authur;
-            //webBrowser1.Navigate("https://youtube.com/");
-            webBrowser1.DocumentText = "<html>"
-                                       + "<head><meta http-equiv='X-UA-Compatible' content='IE=11' />"
-                                       + "<body>" + "\r\n"
-                                       + "<iframe src=\"" + "https://www.youtube.com/embed/1wYNFfgrXTI?ecver=2" + "\" width=\"600\" height=\"500\" frameborder=\"0\" allow = \"autoplay; encrypted - media\" allowfullscreen></iframe>"
-                                         + "</body></html>";
-            //< div style = "position:relative;height:0;padding-bottom:81.73%" >< iframe src = "https://www.youtube.com/embed/1wYNFfgrXTI?ecver=2" style = "position:absolute;width:100%;height:100%;left:0" width = "440" height = "360" frameborder = "0" allow = "autoplay; encrypted-media" allowfullscreen ></ iframe ></ div >
-
-            //string page =
-            //                "<html>"
-            //               + "<head><meta http-equiv='X-UA-Compatible' content='IE=11' />"
-            //               + "<body>" + "\r\n"
-            //               + "<iframe src=\"" + url + "\" width=\"260\" height=\"115\" frameborder=\"0\" allowfullscreen></iframe>"
-            //               + "</body></html>";
-
-            //this.webBrowser1.DocumentText = string.Format(page);
         }
 
-        private void PlayRandomMusic_Btn_Click(object sender, EventArgs e)
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-             //-------------------//
-            // play random music //
-           //-------------------//
-
-            //Random rnd = new Random();
-            //int Get_randVideo = rnd.Next();
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
         }
 
-        private void Next_Page_Search_Btn_Click(object sender, EventArgs e)
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            youtubePageNumber++;
-            Search_Btn_Click(null, null);
+            HtmlElementCollection buttons = webBrowser1.Document.GetElementsByTagName("button");
+            HtmlElementCollection htmlElements = webBrowser1.Document.GetElementsByTagName("a"); 
+            //webBrowser1.Document.Body.Style = "zoom:110%;";
+            //foreach (HtmlElement elem in buttons)
+            //{
+            //    if (elem.OuterHtml.ToString().Contains("Full screen (f)") || elem.GetAttribute("title") == "Full screen (f)")  // keep this
+            //    {
+            //        elem.InvokeMember("click");
+            //    }
+
+            //    if (elem.OuterHtml.ToString().Contains("Theater mode (t)") || elem.GetAttribute("title") == "Cinema mode (t)")  // keep this
+            //    {
+            //        elem.InvokeMember("click");
+            //    }
+            //}
+
         }
-
-        void Download_YoutubeVideo (string link)
+        bool clicked = false;
+        private void Button2_Click(object sender, EventArgs e)
         {
-            var youTube = YouTube.Default; // starting point for YouTube actions
-            var video = youTube.GetVideo(link); // gets a Video object with info about the video
-
-            // path 1
-            string project_path = Application.StartupPath;
-
-            Directory.CreateDirectory(project_path + "/Downloaded Music");
-
-            //path 2
-            string Downloaded_Music_Folder_path = "Downloaded Music";
-
-
-            string path_to_save_To = Path.Combine(project_path, Downloaded_Music_Folder_path);
-
-            File.WriteAllBytes(path_to_save_To + video.FullName, video.GetBytes());
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            index_of_youtubeVideos_List++;
+            clicked = !clicked;
+            if (clicked)
+            {
+                Form1.ActiveForm.Width = 1050;
+                button2.Text = "<";
+            }
+            else
+            {
+                Form1.ActiveForm.Width = 697;
+                button2.Text = ">";
+            }
         }
     }
 }
